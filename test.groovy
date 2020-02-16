eafc40867ec4c634a83756f2e54b743f3d168c40
Your Name <walid.8000@hotmail.fr>
2020-02-16 19:58:58 +0100

    ddd

--- test.groovy
+++ test.groovy
@@ -2,15 +2,19 @@ import java.io.*
 
 
 
- example1 = { List<String> list  ->
+def example1() {
+
+    def outputLines = [
+            'Line one of output example',
+            'Line two of output example',
+            'Line three of output example'
+    ]
 
 
-
-    def newFile = new File("/var/lib/jenkins/workspace/groovy/vars" ,"test.txt").createNewFile()
 
         def newFile1 = new File("/var/lib/jenkins/workspace/groovy/vars" ,"test.txt").withWriter {
 
-            writer -> $list.each {
+            writer -> outputLines.each {
                 line -> writer.writeLine(line)
 
             }
