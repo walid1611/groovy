b6b3523f7d7f6437d3fee88e08195ce87a4037d2
Your Name <walid.8000@hotmail.fr>
2020-02-16 17:30:20 +0100

    ddd

--- test.groovy
+++ test.groovy
@@ -1,7 +1,12 @@
 def example1() {
+
+
+    try {
     def lis  = new File( "/var/lib/jenkins/workspace/groovy",'haiku.txt') as String []
     println lis
-
+    } catch(e) {
+        echo 'There was an error'
+    }
 }
 
 def example2() {
@@ -9,3 +14,7 @@ def example2() {
 }
 
 return this
+
+
+
+    //Method Requiring Script Approval Here
